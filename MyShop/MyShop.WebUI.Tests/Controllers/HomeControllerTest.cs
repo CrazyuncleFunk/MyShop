2554a6e9ff66f5757bc779701cc1f4a3be8082c8
Erin Dunn <erindunn@live.co.uk>
2020-02-18 18:05:48 +0000

    Basket Tests

--- MyShop/MyShop.WebUI.Tests/Controllers/HomeControllerTest.cs
+++ MyShop/MyShop.WebUI.Tests/Controllers/HomeControllerTest.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
 using MyShop.WebUI;
 using MyShop.WebUI.Controllers;
 
@@ -12,28 +15,20 @@ namespace MyShop.WebUI.Tests.Controllers
     [TestClass]
     public class HomeControllerTest
     {
-        [TestMethod]
-        public void Index()
+        [TestMethod]public void IndexPageDoesReturnProducts()
         {
-           
+            IRepository<Product> productContext = new Mocks.MockContext<Product>();
+            IRepository<ProductCatagory> productCatagoryContext = new Mocks.MockContext<ProductCatagory>();
+
+            productContext.Insert(new Product());
+
+            HomeController controller = new HomeController(productContext,productCatagoryContext);
+            var result = controller.Index() as ViewResult;
+            var viewModel = (ProductListViewModel)result.ViewData.Model;
+
+            Assert.AreEqual(1, viewModel.product.Count());
         }
 
-        [TestMethod]
-        public void About()
-        {
-        }
-
-        [TestMethod]
-        public void Contact()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.Contact() as ViewResult;
-
-            // Assert
-            Assert.IsNotNull(result);
-        }
+      
     }
 }
